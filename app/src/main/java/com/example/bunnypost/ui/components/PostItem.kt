9dd75841f1a4c05bdea93178000b0c6dd928914f
AzharLuthfi <azhar.luthfiadi@gmail.com>
2025-06-10 13:01:00 +0700

    feat: ui like dan comment untuk post item

--- app/src/main/java/com/example/bunnypost/ui/components/PostItem.kt
+++ app/src/main/java/com/example/bunnypost/ui/components/PostItem.kt
@@ -1,14 +1,25 @@
 package com.example.bunnypost.ui.components
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.bunnypost.data.remote.model.Post
+
+import androidx.compose.material.icons.filled.Chat
+
+
+
+
+
 
 @Composable
 fun PostItem(post: Post) {
@@ -31,6 +42,28 @@ fun PostItem(post: Post) {
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = post.content, style = MaterialTheme.typography.bodyMedium)
+
+            Spacer(modifier = Modifier.height(12.dp)) // Tambahan agar ada jarak
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = Icons.Filled.Favorite,
+                    contentDescription = "Like",
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "${post.likes.size}")
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Icon(
+                    imageVector = Icons.Filled.Chat,
+                    contentDescription = "Comment",
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = "${post.comments.size}")
+            }
         }
     }
 }
